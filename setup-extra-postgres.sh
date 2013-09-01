db506a986e23d252ee63740053dfcca17d36846c
Faisal Memon <faisal@perivalebluebell.com>
2013-09-01 19:49:27 +0000

    update postgres script for both staging and production

--- setup-extra-postgres.sh
+++ setup-extra-postgres.sh
@@ -1,3 +1,11 @@
-heroku addons:add heroku-postgresql:dev --app faisalmemon-bitstarter-s-mooc
-heroku pg:promote  `heroku config --app faisalmemon-bitstarter-s-mooc | grep HEROKU_POSTGRESQL | cut -f1 -d':'` --app faisalmemon-bitstarter-s-mooc
-heroku plugins:install git://github.com/ddollar/heroku-config.git --app faisalmemon-bitstarter-s-mooc
+#!/bin/bash
+
+function add_postgress()
+{
+heroku addons:add heroku-postgresql:dev --app $1
+heroku pg:promote  `heroku config --app $1 | grep HEROKU_POSTGRESQL | cut -f1 -d':'` --app $1
+heroku plugins:install git://github.com/ddollar/heroku-config.git --app $1
+}
+
+add_postgress faisalmemon-bitstarter-s-mooc
+add_postgress faisalmemon-bitstarter-mooc
